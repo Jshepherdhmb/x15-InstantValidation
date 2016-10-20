4ab234033d79cef00f627fd2394437356b2b1ae7
Jordan Shepherd <jshepherd11@mail.csuchico.edu>
2016-10-19 17:27:11 -0700

    finished

--- form-validation.js
+++ form-validation.js
@@ -1,31 +1,57 @@
-function validate(){
-  //Grab the user's input and store in variables
+function register() {
+		document.getElementById("usernameGroup").classList.remove("has-success");
+		document.getElementById("passwordGroup").classList.remove("has-success");
+		document.getElementById("usernameGroup").classList.remove("has-error");
+		document.getElementById("passwordGroup").classList.remove("has-error");
+  		document.getElementById("passwordError").innerHTML="";
+	validateUsername();
+	validatePassword();
+
+}
+
+function validateUsername() {
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
-  var password = "password"
+  var password = "password";
 if(userEntered.length < 6 || userEntered.indexOf(' ') > 0 || userEntered == passEntered){
 
-  //Show message that there is an error with the username...
-  	document.getElementById("usernameError").innerHTML="Bad username.";
- 	document.getElementById("usernameError").classList.remove("hidden-message");
-  	document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
- 	document.getElementById("usernameGroup").classList.add("has-error");
+ 		 //Show message that there is an error with the username...
+  			document.getElementById("usernameError").innerHTML="Bad username.";
+ 					document.getElementById("usernameError").classList.remove("hidden-message");
+  			document.getElementById("usernameError").classList.add("shown-message");
+ 		 //Turn the username items red
+ 			document.getElementById("usernameGroup").classList.add("has-error");
 }else if (userEntered.length >= 6 && userEntered.indexOf(' ') < 1 && userEntered !=passEntered){
- //Turn the username items to green
-	document.getElementById("usernameGroup").classList.add("has-success");
+ 		//Turn the username items to green
+			document.getElementById("usernameGroup").classList.add("has-success");
 }
-if (passEntered.length < 6 || passEntered == "password"|| passEntered == "PASSWORD" ||  userEntered == passEntered || passEntered.length > 20 || userEntered.toUpperCase == passEntered.toUpperCase || userEntered.toLowerCase == passEntered.toLowerCase) {
-  //Show message that there is an error with the password...
-  	document.getElementById("passwordError").innerHTML="Bad password.";
- 	 document.getElementById("passwordError").classList.remove("hidden-message");
- 	 document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
- 	 document.getElementById("passwordGroup").classList.add("has-error");
 }
-else if (passEntered.length >= 6 && passEntered != "password" && passEntered != "PASSWORD" && passEntered.length <= 20 && userEntered.toUpperCase != passEntered.toUpperCase && userEntered.toLowerCase != passEntered.toLowerCase){
-  //Turn the password items green
 
- 	 document.getElementById("passwordGroup").classList.add("has-success");
+function validatePassword() {
+  var userEntered = document.getElementById("user").value;
+  var passEntered = document.getElementById("pass").value;
+  var password = "password";
+
+if (passEntered.length < 6 || passEntered == "password"|| passEntered == "PASSWORD" ||  userEntered == passEntered || passEntered.length > 20 || userEntered.toUpperCase() == passEntered.toUpperCase() || userEntered.toLowerCase() == passEntered.toLowerCase()) {
+  		//Show message that there is an error with the password...
+  			document.getElementById("passwordError").innerHTML="Bad password.";
+ 	 		document.getElementById("passwordError").classList.remove("hidden-message");
+ 	 		document.getElementById("passwordError").classList.add("shown-message");
+  		//Turn the password items red
+ 			document.getElementById("passwordGroup").classList.add("has-error");
+
+}else{
+  		//Turn the password items green
+
+ 			 document.getElementById("passwordGroup").classList.add("has-success");
+}	
 }
-}
+
+
+
+
+
+
+
+
+
